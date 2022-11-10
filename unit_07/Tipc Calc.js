9f6122ea4f514ed6e8f4bd9008d4ff8caa3d6a23
L0lip <114414422+L0lip@users.noreply.github.com>
2022-11-10 14:58:36 +0000

    aesthetic

---
+++ unit_07/Tipc Calc.js
@@ -0,0 +1,49 @@
+var amount;
+
+var tip;
+var tip1;
+var tip2;
+
+function submit() {
+    amount = document.getElementById("Amount").value;
+    alert("Amount has been entered already");
+    return amount;
+    
+}
+
+
+function Tip5() {
+    tip = (amount / 100) * 5;
+    tip = tip.toFixed(2);
+    document.getElementById('input').innerHTML = "£" + tip;
+}
+
+
+function Tip10() {
+     tip = (amount / 100) * 10;
+     tip = tip.toFixed(2);
+    document.getElementById('input').innerHTML = "£" + tip;
+}
+
+
+function Tip25() {
+     tip = (amount / 100) * 25;
+     tip = tip.toFixed(2);
+     document.getElementById('input').innerHTML = "£" + tip;
+
+}
+
+
+function bigTip() {
+    tip1=(amount/ 100)*25;
+    tip2=(amount/100)*10 +2;
+    if ( tip1 > tip2 ) {
+        tip= tip1;
+        tip= tip.toFixed(2);
+        document.getElementById('input').innerHTML = "£" + tip;
+} else {
+    tip = tip2;
+    tip = tip.toFixed(2);
+    document.getElementById('input').innerHTML = "£" + tip;
+}
+}
